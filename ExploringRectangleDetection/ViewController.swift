0222a601ec93c64fa516f54fca32eeb5392bcf7a
Jonathan <jonathancbadger@gmail.com>
2020-01-31 13:54:38 -0600

    Initial Commit

---
+++ ExploringRectangleDetection/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExploringRectangleDetection
+//
+//  Created by Jonathan Badger on 1/31/20.
+//  Copyright © 2020 Jonathan Badger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
